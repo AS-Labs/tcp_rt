71e6d8b5a3f8d60a6e0dac913357a59eee1f6451
as-labs <46945604+AS-Labs@users.noreply.github.com>
2024-09-19 11:58:09 +0300

    Update tcp_rt.c
    Rewrote the implementation to parse a file line by line.

--- tcp_rt.c
+++ tcp_rt.c
@@ -7,17 +7,11 @@
 #include <sys/time.h>
 #include <time.h>
 
-
-int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        printf("Example: %s SERVERIP PORT\n", argv[0]);
-        return 1;
-    }
-
+int connection_lat(char *ip_var, char *port_var) {
     // first arg is the target servers IP
-    const char *server_ip = argv[1];
+    const char *server_ip = ip_var;
     // second arg is the target port, converting it from string to int
-    int port = atoi(argv[2]);
+    int PORT = atoi(port_var);
 
     // Creating a socket, and sock is where we place the fd from the socket syscall.
     // The arguments of socket(int domain, int type, int protocol)
@@ -30,20 +24,21 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    // in defining the sockaddr_in as server_addr
+    // in defining the sockaddr_in as SERVERIP
     // we get access to the struct of sockaddr_in
     // which gives us access to the below:
     // short            sin_family;
     // unsigned short   sin_port;   
     // struct in_addr   sin_addr;
     // char             sin_zero[8];
-    struct sockaddr_in server_addr;
-    // specify the server_addr as IPv4
-    server_addr.sin_family = AF_INET;
+
+    struct sockaddr_in SERVERIP;
+    // specify the SERVERIP as IPv4
+    SERVERIP.sin_family = AF_INET;
     // convert the unsigned short int from host byte order to network byte order
-    server_addr.sin_port = htons(port);
+    SERVERIP.sin_port = htons(PORT);
     // convert IPv4 from text to binary as a pointer to the character string for IPv4
-    inet_pton(AF_INET, server_ip, &(server_addr.sin_addr));
+    inet_pton(AF_INET, server_ip, &(SERVERIP.sin_addr));
 
     struct timeval start_time, end_time;
 
@@ -51,13 +46,10 @@ int main(int argc, char *argv[]) {
     // timezone is given as null, as that is not required?
     gettimeofday(&start_time, NULL);
 
-    // initiate a connection on a socket with the connect() syscall
-    // arg1 = int fd;
-    // arg2 = struct sockaddr 
-    // arg3 = addr length
-    // if addr length is -1 then close the socket fd and return 1
+    // initiate a connection on a socket with the connect() syscall.
+    // if addr length is -1 then close the socket fd and return 1.
     // handling this for if the connection is refused or does not exist.
-    if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+    if (connect(sock, (struct sockaddr *)&SERVERIP, sizeof(SERVERIP)) == -1) {
         perror("connect");
         close(sock);
         return 1;
@@ -70,12 +62,45 @@ int main(int argc, char *argv[]) {
     close(sock);
 
     // converting the times to ms
-    long start_ms = start_time.tv_sec * 1000 + start_time.tv_usec / 1000;
-    long end_ms = end_time.tv_sec * 1000 + end_time.tv_usec / 1000;
-    long response_time = end_ms - start_ms;
+    long STR_MS = start_time.tv_sec * 1000 + start_time.tv_usec / 1000;
+    long END_MS = end_time.tv_sec * 1000 + end_time.tv_usec / 1000;
+    long response_time = END_MS - STR_MS;
 
-    // our output
-    printf("%lu,%s,%d,%ld\n",(unsigned long)time(NULL) ,server_ip, port, response_time);
+    // output
+    printf("%lu,%s,%d,%ld\n",(unsigned long)time(NULL) ,server_ip, PORT, response_time);
 
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Example: %s filename\n", argv[0]);
+        return 1;
+    }
+
+    // Create file read loop where server IP and port are parsed and sent through a function that takes the two as arguments
+
+    FILE *fp;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    char *item1;
+    char *item2;
+    char *delimiter = ":";
+
+    fp = fopen(argv[1], "r");
+    if (fp == NULL)
+        exit(EXIT_FAILURE);
+
+    printf("time(unix),server ip,port,latency(ms)\n");
+    while ((read = getline(&line, &len, fp)) != -1) {
+        item1 = strtok(line, delimiter);
+        item2 = strtok(NULL, delimiter);
+        connection_lat(item1, item2);
+    }
+
+    fclose(fp);
+    if (line)
+        free(line);
+    exit(EXIT_SUCCESS);
     return 0;
 }
