ba75cc83e2a58bd7b824e74dbaba434e1f07e202
as-labs <a.alshaiji@protonmail.com>
2024-09-21 11:30:32 +0300

    fixed C implementation in tcp_rt_test.c , more checks needed.

--- tcp_rt_test.c
+++ tcp_rt_test.c
@@ -1,80 +1,98 @@
 #define _GNU_SOURCE
-#include <errno.h>
-#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <netdb.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <sys/socket.h>
 #include <sys/time.h>
-#include <time.h>
-#include <pthread.h>
 
-int main(int argc, char **argv) {
-    
-    FILE *fp;
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t read;
-    char *item1;
-    char *item2;
-    char *delimiter = ":";
+void print_usage(const char *program_name) {
+    printf("Usage: %s <filename>\n", program_name);
+}
 
-    u_short port;                /* user specified port number */
-    char *addr;                  /* will be a pointer to the address */
-    struct sockaddr_in address;  /* the libc network address data structure */
-    short int sock = -1;         /* file descriptor for the network socket */
-    fd_set fdset;
+long get_unix_time() {
     struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return tv.tv_sec;
+}
 
-    if (argc != 2) {
-        fprintf(stderr, "Usage %s filename\n", argv[0]);
-        return EXIT_FAILURE;
+double calculate_latency(struct timeval start, struct timeval end) {
+    return (end.tv_sec - start.tv_sec) * 1000.0 + (end.tv_usec - start.tv_usec) / 1000.0;
+}
+
+int connect_to_server(const char *ip, int port, int timeout_seconds) {
+    struct sockaddr_in server_address;
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sockfd < 0) {
+        return -1;
     }
 
-    fp = fopen(argv[1], "r");
-    if (fp == NULL)
-        exit(EXIT_FAILURE);
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(port);
 
-    printf("time(unix),server ip,port,latency(ms)\n");
-    while ((read = getline(&line, &len, fp)) != -1) {
-        item1 = strtok(line, delimiter);
-        item2 = strtok(NULL, delimiter);
-        connection_lat(item1, item2);
+    if (inet_pton(AF_INET, ip, &server_address.sin_addr) <= 0) {
+        close(sockfd);
+        return -1;
     }
 
+    struct timeval timeout;
+    timeout.tv_sec = 1;
+    timeout.tv_usec = 0;
 
-    port = atoi(argv[1]);
-    addr = argv[2];
+    setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
+    setsockopt(sockfd, SOL_SOCKET, SO_SNDTIMEO, &timeout, sizeof(timeout));
 
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = inet_addr(addr); /* assign the address */
-    address.sin_port = htons(port);            /* translate int2port num */
+    if (connect(sockfd, (struct sockaddr *)&server_address, sizeof(server_address)) < 0) {
+        close(sockfd);
+        return -1;
+    }
 
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-    fcntl(sock, F_SETFL, O_NONBLOCK);
+    return sockfd;
+}
 
-    connect(sock, (struct sockaddr *)&address, sizeof(address));
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        print_usage(argv[0]);
+        return 1;
+    }
 
-    FD_ZERO(&fdset);
-    FD_SET(sock, &fdset);
-    tv.tv_sec = 2;             /* 2 second timeout */
-    tv.tv_usec = 0;
+    FILE *file = fopen(argv[1], "r");
+    if (!file) {
+        perror("Failed to open file");
+        return 1;
+    }
 
-    if (select(sock + 1, NULL, &fdset, NULL, &tv) == 1)
-    {
-        int so_error;
-        socklen_t len = sizeof so_error;
+    char line[256];
 
-        getsockopt(sock, SOL_SOCKET, SO_ERROR, &so_error, &len);
+    printf("Time,ip,port,latency\n");
 
-        if (so_error == 0) {
-            printf("%s:%d is open\n", addr, port);
+    while (fgets(line, sizeof(line), file)) {
+        char *ip = strtok(line, ":");
+        char *port_str = strtok(NULL, ":");
+        int port = atoi(port_str);
+
+        struct timeval start, end;
+        gettimeofday(&start, NULL);
+
+        int sockfd = connect_to_server(ip, port, 1);
+        if (sockfd < 0) {
+            long current_time = get_unix_time();
+            printf("%ld,%s,%d,timeout\n", current_time, ip, port);
+            continue;
         }
+
+        gettimeofday(&end, NULL);
+        double latency = calculate_latency(start, end);
+
+        long current_time = get_unix_time();
+        printf("%ld,%s,%d,%.2f\n", current_time, ip, port, latency);
+
+        close(sockfd);
     }
 
-    close(sock);
+    fclose(file);
     return 0;
 }
 
